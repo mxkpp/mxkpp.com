6d70b377ee3241bed8f23ad2b8d1f1cfe51ef7b4
Max Kipp <code@mxkpp.com>
2023-02-19 22:40:04 -0500

    update local deploy script

--- deploy-local.sh
+++ deploy-local.sh
@@ -2,4 +2,9 @@
 
 set -euo pipefail
 
-docker compose -f docker-compose.local.yml up
+YML="docker-compose.local.yml"
+
+docker compose -f "${YML}" down
+rm -rf "_site" && mkdir -m a=rwx "_site"
+docker compose -f "${YML}" build
+docker compose -f "${YML}" up
