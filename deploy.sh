d02b547c3981b1da22670c2a92d8cde2d9761e59
Max Kipp <code@mxkpp.com>
2023-02-22 04:47:20 +0000

    run container as detached

--- deploy.sh
+++ deploy.sh
@@ -7,4 +7,4 @@ YML="docker-compose.yml"
 docker compose -f "${YML}" down
 rm -rf "www" && mkdir -m a=rwx "www"
 docker compose -f "${YML}" build
-docker compose -f "${YML}" up
+docker compose -f "${YML}" up -d
