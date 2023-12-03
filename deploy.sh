0034fc189d1e219d9dafcb5cb5e44e8b75fa921b
Max Kipp <code@mxkpp.com>
2023-12-03 15:57:17 -0500

    change deploy default build setting

--- deploy.sh
+++ deploy.sh
@@ -7,11 +7,11 @@ YML="docker-compose.yml"
 docker compose -v -f "${YML}" down
 rm -rf "www" && mkdir -m a=rwx "www"
 
-# New release
-docker compose -f "${YML}" build --no-cache
+# Initial standup or hotfix
+docker compose -f "${YML}" build
 
-# # Hotfix
-# docker compose -f "${YML}" build
+# # New release
+# docker compose -f "${YML}" build --no-cache
 
 # All
 docker compose -f "${YML}" up -d
